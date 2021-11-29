d735925069283f27f29f786d01ae359ef734deec
Rawezhham176 <rawezhhame@gmail.com>
2021-11-29 01:23:26 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Header } from './components'
+import { makeStyles } from '@material-ui/core'
+import { Coins } from './pages';
+import  Homepage  from './pages/Homepage';
+
 
 function App() {
+
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: '#14161a',
+      color: 'white',
+      minHeight: '100vh'
+    }
+  }))
+
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <div className={classes.App}>
+      <Header />
+        <Routes>
+          <Route path="/" component={ Homepage } exact />
+          <Route path="/coins/:id" component={ Coins } exact />
+        </Routes>
+ </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/CryptoContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useState, useEffect, useContext } from 'react'
+
+const Crypto = createContext()
+
+const CryptoContext = ({children}) => {
+    const [Currency, setCurrency] = useState("EUR")
+    const [Symbol, setSymbol] = useState('€')
+    
+    useEffect(() => {
+        if(Currency === "EUR") setSymbol('€')
+        else if (Currency === "USD") setSymbol('$')
+    }, [Currency])
+
+    return (
+      <Crypto.Provider value={{Currency, Symbol, setCurrency}}>
+          {children}
+      </Crypto.Provider>
+
+    )
+}
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+   return useContext(Crypto)
+}
